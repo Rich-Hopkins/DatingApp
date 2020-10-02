1cff53f89cc55faba7249d4cc1d74773b79d5d99
hopmedic@hotmail.com <hopmedic@hotmail.com>
2020-10-01 23:53:13 -0400

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,12 +3,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-   
+
     public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
@@ -17,6 +18,7 @@ namespace API.Controllers
             _context = context;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -25,8 +27,9 @@ namespace API.Controllers
 
 
         //api/users/3
+        [Authorize]
         [HttpGet("{id}")]
-        public async Task< ActionResult<AppUser>> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
         }
---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,20 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerService
+    {
+        public static IServiceCollection AddSwaggerServices
+        (this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                { Title = "API", Version = "v1" });
+            });
+            return services;
+        }
+    }
+}
