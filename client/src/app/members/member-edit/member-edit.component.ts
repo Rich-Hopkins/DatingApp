b829dfb873b1e8c84d05402cb6e5d11c2008120c
hopmedic@hotmail.com <hopmedic@hotmail.com>
2021-01-17 21:14:16 -0500

    Updating user profile from UI

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -13,14 +13,14 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  @ViewChild('editForm') editForm:NgForm;
+  @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
-@HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
-  if(this.editForm.dirty){
-    $event.returnValue = true;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
   }
-}
 
   constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
@@ -36,10 +36,10 @@ export class MemberEditComponent implements OnInit {
     })
   }
 
-  updateMember(){
-    console.log(this.member);
-    this.toastr.success('Profile updated successfully');
-    this.editForm.reset(this.member);
+  updateMember() {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm.reset(this.member);
+    })
   }
-
 }
