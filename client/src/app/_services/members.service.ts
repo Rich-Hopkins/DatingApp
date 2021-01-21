b65b5d43a7c19bd8756e1414451a4eaeb8b9a18e
hopmedic@hotmail.com <hopmedic@hotmail.com>
2021-01-20 23:52:54 -0500

    Client setting main photo

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -10,12 +10,12 @@ import { Member } from '../_models/member';
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
-members: Member[] = [];
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    if(this.members.length > 0) return of(this.members);
+    if (this.members.length > 0) return of(this.members);
     return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
       map(members => {
         this.members = members;
@@ -25,18 +25,21 @@ members: Member[] = [];
   }
 
   getMember(username: string) {
-    const member = this.members.find(x=>x.username === username);
+    const member = this.members.find(x => x.username === username);
     if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(member: Member){
+  updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      map(()=> {
+      map(() => {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
     );
   }
 
+  setMainPhoto(photoId: number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
 }
