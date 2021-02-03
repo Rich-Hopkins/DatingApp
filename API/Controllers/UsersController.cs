9d3f85af04b71d0b8032bf46eabb1ea874f41105
hopmedic@hotmail.com <hopmedic@hotmail.com>
2021-02-02 22:29:09 -0500

    Added caching back to the application

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -43,6 +43,7 @@ namespace API.Controllers
 
             var users = await _userRepository.GetMembersAsync(userParams);
 
+
             Response.AddPaginationHeader(users.CurrentPage, users.PageSize, users.TotalCount, users.TotalPages);
 
             return Ok(users);
