a7f575826ee99b588cd696153b0295d50ded70af
hopmedic@hotmail.com <hopmedic@hotmail.com>
2021-02-06 23:26:17 -0500

    Added MessageRepository

---
+++ API/Data/MessageRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly DataContext _context;
+        public MessageRepository(DataContext _context)
+        {
+            _context = _context;
+        }
+
+        public void AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public Task<PagedList<MessageDto>> GetMessagesForUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientUserId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
