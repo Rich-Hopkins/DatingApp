bb7efce46f61bfc8cb4e5a8f1a40e9f14f5a112e
hopmedic@hotmail.com <hopmedic@hotmail.com>
2021-02-15 21:00:47 -0500

    Toast message to notify user of new message

--- client/src/app/_services/presence.service.ts
+++ client/src/app/_services/presence.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { Toast, ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
 
@@ -14,34 +16,41 @@ export class PresenceService {
   private onlineUsersSource = new BehaviorSubject<string[]>([]);
   onlineUsers$ = this.onlineUsersSource.asObservable();
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private router: Router) { }
 
-  createHubConnection(user: User){
+  createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder()
       .withUrl(this.hubUrl + 'presence', {
         accessTokenFactory: () => user.token
-      })    
+      })
       .withAutomaticReconnect()
       .build();
 
-      this.hubConnection
-        .start()
-        .catch(error => console.log(error));
+    this.hubConnection
+      .start()
+      .catch(error => console.log(error));
 
-      this.hubConnection.on('UserIsOnline', username => {
-        this.toastr.info(username + ' has connected');
-      })
+    this.hubConnection.on('UserIsOnline', username => {
+      this.toastr.info(username + ' has connected');
+    })
 
-      this.hubConnection.on('UserIsOffline', username => {
-        this.toastr.info(username + ' has disconnected');
-      })
+    this.hubConnection.on('UserIsOffline', username => {
+      this.toastr.info(username + ' has disconnected');
+    })
 
-      this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
-        this.onlineUsersSource.next(usernames);
-      })
+    this.hubConnection.on('GetOnlineUsers', (usernames: string[]) => {
+      this.onlineUsersSource.next(usernames);
+    })
+
+    this.hubConnection.on('NewMessageReceived', ({ username, knownAs }) => {
+      this.toastr.info("New message from " + knownAs)
+        .onTap
+        .pipe(take(1))
+        .subscribe(() => this.router.navigateByUrl('/members/' + username + '?tab=3'));
+    })
   }
 
-  stopHubConnection(){
+  stopHubConnection() {
     this.hubConnection.stop().catch(error => console.log(error));
   }
 }
